728b4d71787abc1dcccd8dca0b56b44ed7103674
wanglibin <wlbin_1019@163.com>
2024-01-21 12:02:51 +0800

    ky

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -12,7 +12,7 @@ import { stringify } from 'querystring';
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://121.41.226.62' : undefined,
+  prefix: process.env.NODE_ENV === 'production' ? 'http://121.41.226.62:80' : undefined,
   // requestType: 'form',
 });
 
